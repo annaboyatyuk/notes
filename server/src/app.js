31f7b77eb9f6b309b388db829d5c1f3d068d9b15
annaboyatyuk <annaboyatyuk@gmail.com>
2019-03-01 00:25:26 -0800

    testing

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,25 @@
 'use strict';
 
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
 import router from './api/api.js';
+
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/404.js';
 
 let app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(router);
+
+app.use(notFound);
+app.use(errorHandler);
 
 let isRunning = false;
 let server;
